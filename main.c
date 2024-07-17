c0cf49ad4af38e30cebd21a82cbcae9dbbb1de37
Mihai Gătejescu <gus666xe@gmail.com>
2024-07-17 22:22:49 +0300

    Move the cursor

--- main.c
+++ main.c
@@ -1,15 +1,44 @@
 #include <ncurses.h>
+#include <stdlib.h>
 
 int main()
 {
     char *text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus congue convallis laoreet. Aliquam at pulvinar magna. Mauris rutrum, quam vitae tincidunt tempus, metus risus semper nisi, a porta nunc est et odio. Proin id lorem sed elit consequat vulputate id ac augue. Ut ultrices lectus nec congue condimentum. Phasellus a est a ex porttitor luctus in sed velit. Fusce quis aliquet nulla. Sed quam ante, rutrum id pulvinar non, feugiat nec lectus. Nulla pellentesque nibh nec consequat pellentesque. In sagittis pellentesque lobortis. Nam eu nibh ut quam imperdiet luctus in eget metus. Vestibulum pellentesque et ante vel dapibus. Proin venenatis massa id malesuada placerat. Suspendisse sollicitudin, sem quis consequat porttitor, lacus nulla consectetur nisl, vel viverra ante eros vel tortor. In ut urna vel mi convallis rutrum.\0";
 
     initscr();
+    noecho();
 
     addstr(text);
     move(0, 0);
 
-    getch();
+    int ch;
+    while (ch = getch())
+    {
+        int y, x;
+        getyx(stdscr, y, x);
+        switch (ch)
+        {
+            case 'q':
+                endwin();
+                exit(0);
+            case 'h':
+                move(y, x - 1);
+                break;
+            case 'j':
+                move(y + 1, x);
+                break;
+            case 'k':
+                move(y - 1, x);
+                break;
+            case 'l':
+                move(y, x + 1);
+                break;
+            default:
+                break;
+
+        }
+
+    }
 
     endwin();
 }
