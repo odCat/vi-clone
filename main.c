f8f4e36d88953dfcb4e8222bcb823463aae6518c
Mihai Gătejescu <gus666xe@gmail.com>
2024-07-17 21:58:54 +0300

    Print random text to the screen

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <ncurses.h>
+
+int main()
+{
+    char *text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus congue convallis laoreet. Aliquam at pulvinar magna. Mauris rutrum, quam vitae tincidunt tempus, metus risus semper nisi, a porta nunc est et odio. Proin id lorem sed elit consequat vulputate id ac augue. Ut ultrices lectus nec congue condimentum. Phasellus a est a ex porttitor luctus in sed velit. Fusce quis aliquet nulla. Sed quam ante, rutrum id pulvinar non, feugiat nec lectus. Nulla pellentesque nibh nec consequat pellentesque. In sagittis pellentesque lobortis. Nam eu nibh ut quam imperdiet luctus in eget metus. Vestibulum pellentesque et ante vel dapibus. Proin venenatis massa id malesuada placerat. Suspendisse sollicitudin, sem quis consequat porttitor, lacus nulla consectetur nisl, vel viverra ante eros vel tortor. In ut urna vel mi convallis rutrum.\0";
+
+    initscr();
+
+    addstr(text);
+    move(0, 0);
+
+    getch();
+
+    endwin();
+}
