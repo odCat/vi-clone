297afb39b2911b946de850b280ffccf68964cca3
Mihai Gătejescu <mgatejescu@pm.me>
2024-11-02 20:51:52 +0200

    Add the error header

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "error.h"
+
 WINDOW *status;
 
 void enter_insert_mode(WINDOW *edit, int y, int x);
@@ -12,13 +14,12 @@ void search(WINDOW *command);
 void enter_command_mode(WINDOW *command);
 void clear_command(WINDOW *command);
 
-void errExit(char *er);
 
 int main()
 {
     int fd = open("./frankenstein.txt", O_RDONLY);
     if (fd == -1)
-        errExit("Error closing file");
+        errExit("Error opening file");
 
     char *text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus congue convallis laoreet. Aliquam at pulvinar magna. Mauris rutrum, quam vitae tincidunt tempus, metus risus semper nisi, a porta nunc est et odio. Proin id lorem sed elit consequat vulputate id ac augue. Ut ultrices lectus nec congue condimentum. Phasellus a est a ex porttitor luctus in sed velit. Fusce quis aliquet nulla. Sed quam ante, rutrum id pulvinar non, feugiat nec lectus. Nulla pellentesque nibh nec consequat pellentesque. In sagittis pellentesque lobortis. Nam eu nibh ut quam imperdiet luctus in eget metus. Vestibulum pellentesque et ante vel dapibus. Proin venenatis massa id malesuada placerat. Suspendisse sollicitudin, sem quis consequat porttitor, lacus nulla consectetur nisl, vel viverra ante eros vel tortor. In ut urna vel mi convallis rutrum.\0";
 
