f9405e773accf7aa7a38b44a50102efd9df9c78d
Mihai Gătejescu <mgatejescu@pm.me>
2024-11-27 00:06:12 +0200

    Read and text from file

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <curses.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <ncurses.h>
 #include <stdlib.h>
@@ -22,9 +23,27 @@ void clear_command(WINDOW *command);
 
 int main()
 {
-    int fd = open("./frankenstein.txt", O_RDONLY);
-    if (fd == -1)
+    // int fd = open("./frankenstein.txt", O_RDONLY);
+    // if (fd == -1)
+    //     errExit("Error opening file");
+
+    int c;
+    long len;
+
+    FILE *fp = fopen("./frankenstein.txt", "r+");
+    if (fp == NULL)
         errExit("Error opening file");
+    while ((c = getc(fp)) != EOF)
+        ++len;
+    char *buffer = (char *)malloc(1000 * sizeof(char));
+
+    rewind(fp);
+    char *temp = buffer;
+    for (int i = 0; i < 1000; ++i)
+        *temp++ = (char) getc(fp);
+
+
+    fclose(fp);
 
     char *text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus congue convallis laoreet. Aliquam at pulvinar magna. Mauris rutrum, quam vitae tincidunt tempus, metus risus semper nisi, a porta nunc est et odio. Proin id lorem sed elit consequat vulputate id ac augue. Ut ultrices lectus nec congue condimentum. Phasellus a est a ex porttitor luctus in sed velit. Fusce quis aliquet nulla. Sed quam ante, rutrum id pulvinar non, feugiat nec lectus. Nulla pellentesque nibh nec consequat pellentesque. In sagittis pellentesque lobortis. Nam eu nibh ut quam imperdiet luctus in eget metus. Vestibulum pellentesque et ante vel dapibus. Proin venenatis massa id malesuada placerat. Suspendisse sollicitudin, sem quis consequat porttitor, lacus nulla consectetur nisl, vel viverra ante eros vel tortor. In ut urna vel mi convallis rutrum.\0";
 
@@ -46,12 +65,12 @@ int main()
     int y, x;
     WINDOW *edit = newwin(LINES - 3, COLS, 0, 0);
     keypad(edit, TRUE);
-    waddstr(edit, text);
+    waddstr(edit, buffer);
     wmove(edit, 0, 0);
     getyx(edit, y, x);
     wrefresh(edit);
 
-   status = newwin(1, COLS, LINES - 2, 0);
+    status = newwin(1, COLS, LINES - 2, 0);
     wbkgd(status, COLOR_PAIR(1));
 
     mvwprintw(status, 0, 0, "%d,%d", y, x);
@@ -136,8 +155,9 @@ int main()
     }
 
     endwin();
-    if (fd == -1)
-        errExit("Error closing file");
+    // if (fd == -1)
+    //     errExit("Error closing file");
+    free(buffer);
 }
 
 void enter_insert_mode(WINDOW *edit, int y, int x)
